84a877ecc9fa1190e9b57d759d6fc4a93fde044e
Yashasvi-026 <yashasvichauhan26jul@gmail.com>
2026-08-23 01:21:49 +0530

    Add sentence generation functionality

--- backend/api/sentence_api.py
+++ backend/api/sentence_api.py
@@ -1,1 +1,14 @@
+from sentence_generator import generate_sentence
 
+def generate(words):
+    print("WORDS:", words)
+    print("TYPE:", type(words))
+    sentence = generate_sentence(words)
+
+    return {
+
+        "success": True,
+
+        "sentence": sentence
+
+    }
