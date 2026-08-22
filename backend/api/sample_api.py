8379770bd6a01917eeadbdbb6c95bcffe9799774
Yashasvi-026 <yashasvichauhan26jul@gmail.com>
2026-08-23 01:21:13 +0530

    Implement save_sample function for user word samples
    The function saves a sample associated with a user and increments the sample count for a word. If the sample count reaches 45, it starts training the model in a new thread.

--- backend/api/sample_api.py
+++ backend/api/sample_api.py
@@ -1,1 +1,92 @@
+from database import samples, words
+from api.train_api import train_model
+import threading
+from bson import ObjectId
 
+
+def save_sample(user_id, word, landmarks):
+
+    word_data = words.find_one({
+
+        "userId": ObjectId(user_id),
+
+        "word": word
+    })
+
+    if word_data is None:
+
+        return {
+
+            "success": False,
+
+            "message": "Word not found"
+        }
+
+    samples.insert_one({
+
+        "userId": ObjectId(user_id),
+
+        "word": word,
+
+        "landmarks": landmarks
+    })
+
+
+    words.update_one(
+        {
+            "userId": ObjectId(user_id),
+
+            "word": word
+        },
+
+        {
+            "$inc": {
+                "sampleCount": 1
+            }
+        }
+    )
+
+    total_samples = samples.count_documents({
+
+        "userId": ObjectId(user_id),
+
+        "word": word
+
+    })
+
+    if total_samples == 45:
+
+        threading.Thread(
+
+    target=train_model,
+
+    daemon=True
+
+).start()
+
+        return {
+
+        "success": True,
+
+        "training": True,
+
+        "sampleCount": total_samples,
+
+        "requiredSamples": 45,
+
+        "message": "Training Started"
+    }
+
+    return {
+
+        "success": True,
+
+        "training": False,
+
+        "sampleCount": total_samples,
+
+        "requiredSamples": 45,
+
+        "message": "Sample Saved"
+
+    }
